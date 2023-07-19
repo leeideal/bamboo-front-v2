0cfb2f427478fad60cd42d062bd7c01ae5f2e5f3
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2023-07-19 19:47:19 +0900

    feat:pages basic setting

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  
+`
+
+export default function Main(){
+  return(
+    <Container>
+      Here is Main Page!!
+    </Container>
+  )
+}

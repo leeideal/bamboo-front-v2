16c7a23246a34544ce052c41a2d4ba3ef628d143
Seulgigi <2021112414@dgu.ac.kr>
2023-07-29 02:26:12 +0900

    feat: 제보 상세페이지 UI

--- pages/suggestion/[id].jsx
+++ pages/suggestion/[id].jsx
@@ -1,8 +1,57 @@
-import styled from "styled-components";
+import { DetailCommentContainer, DetailCommentCount, DetailCommentNum, DetailContainer, DetailContent, DetailDate, DetailNumber, DetailText, DetailType, DetailCommentHeader, DetailCommentBtn, DetailComment, DetailCommentImg, DetailCommentContent } from "../_styled/detailStyled"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import logo from '../../components/image/logo.png'
+import { useRouter } from "next/router";
 
 export default function SuggestionDetail() {
 
+  const router = useRouter()
+
   return (
-    <>제보 상세페이지</>
+    <DetailContainer>
+      <DetailType>일반 제보</DetailType>
+
+      <DetailContent>
+        <DetailNumber>#1번째 뿌우</DetailNumber>
+        <DetailDate>2023-05-10 16:43</DetailDate>
+        
+        <DetailText>어쩌구 그 분 찾아요!어쩌구 그 분 찾아요!어쩌구 그 분 찾아요!어쩌구 그 분 찾</DetailText>
+      </DetailContent>
+
+      <DetailCommentContainer>
+
+        <DetailCommentHeader>
+          <DetailCommentCount>댓글 2</DetailCommentCount>
+          <DetailCommentBtn
+            onClick={()=>router.push(`/comment`)}
+          >
+            <FontAwesomeIcon icon={faMessage} width={12}/>
+            댓글달기
+          </DetailCommentBtn>
+        </DetailCommentHeader>
+
+        <DetailComment>
+          <DetailCommentNum>
+            댓글1
+            <DetailCommentImg src={logo} />
+          </DetailCommentNum>
+          <DetailCommentContent>
+            여기가 댓글이야 여기
+          </DetailCommentContent>
+        </DetailComment>
+
+        <DetailComment>
+          <DetailCommentNum>
+            댓글1
+            <DetailCommentImg src={logo} />
+          </DetailCommentNum>
+          <DetailCommentContent>
+            여기가 댓글이야 여기
+          </DetailCommentContent>
+        </DetailComment>
+
+      </DetailCommentContainer>
+    </DetailContainer>
   )
 }
